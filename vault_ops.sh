b0021efaa9fda9cd6881f6a500fbd4baa94e77a1
Keza Rutayisire Alicia <kezalicia257@gmail.com>
2025-09-27 05:39:14 -0700

    Adding and Updating secrets

---
+++ vault_ops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+while true; do
+echo  " Secure vault menu"
+echo " 1.Add  secret"
+echo " 2.Update Secret(find and replace)"
+echo " 3. Add log entry"
+echo "4. access keys"
+echo "5.Exit"
+ read -p  "Choose an option(1-5)" choice
+case $choice in
+# add a secret
+1) 
+read -p "Enter a secret" secret
+echo "$secret" >> ~/secure_vault/secrets.txt
+echo "The secret has been added"
+;;
+2)
+read -p "Enter a  secret to replace:" old
+read -p "Enter a new secret" new
+ if grep -q "$old" ~/secure_vault/secrets.txt; then
+sed -i "s/$old/$new/" ~/secure_vault/secrets.txt
+echo "Secret replaced"
+else
+echo "No match found"
+fi
+;;
+*)
+echo "Invalid choice"
+;;
+esac
+done
